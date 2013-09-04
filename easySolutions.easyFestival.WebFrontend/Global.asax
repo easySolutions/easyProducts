21f54d7b68f3148c8ac8ce7ac52b92b075d63816
JohnyWS <Johny@WollerSkovdal.dk>
2013-09-04 10:54:47 +0200

    Basic structure - including demo site

---
+++ easySolutions.easyFestival.WebFrontend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="easySolutions.easyFestival.WebFrontend.MvcApplication" Language="C#" %>
---
+++ easySolutions.easyFestival.WebFrontend/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace easySolutions.easyFestival.WebFrontend
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
